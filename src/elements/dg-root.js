9b73ebf0b7993efdc5b6d2878bad6560decadd3f
Marcus <marcus@valnet.xyz>
2020-09-30 14:12:50 -0400

    background added

--- src/elements/dg-root.js
+++ src/elements/dg-root.js
@@ -1,9 +1,9 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css, unsafeCSS } from 'lit-element';
 import auth from './../lib/auth';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import dgLogo from './../res/DG-logos-01.svg';
 import { LOGIN, DASHBOARD, RESET, SIGNUP } from './../lib/pages';
-
+import background from './../res/dynamic grid backgrounds_transparent 12% (1).png';
 
 class DgRoot extends LitElement {
 
@@ -47,9 +47,19 @@ class DgRoot extends LitElement {
 			}
 
 			.content {
+				width: 100%;
+				height: 100%;
+				background: #E6EDF1;
+			}
+
+			.content .overlay {
+				background: url(${unsafeCSS(background)});
+				background-size: cover;
+				width: 100%;
+				height: 100%;
+
 				display: grid;
 				place-items: center center;
-				background: #E6EDF1;
 			}
 
 			.logo {
@@ -89,34 +99,36 @@ class DgRoot extends LitElement {
 							<dg-header></dg-header>
 						</div>
 						<div class="content">
-							${(() => {
-								switch (this.page) {
-									case LOGIN: {
-										return html`
-											<dg-page-login
-											@signup=${(evt) => this.page = SIGNUP}
-											@forgotPassword=${(evt) => this.page = RESET}
-											></dg-page-login>
-										`;
+							<div class="overlay">
+								${(() => {
+									switch (this.page) {
+										case LOGIN: {
+											return html`
+												<dg-page-login
+												@signup=${(evt) => this.page = SIGNUP}
+												@forgotPassword=${(evt) => this.page = RESET}
+												></dg-page-login>
+											`;
+										}
+										case RESET: {
+											return html`
+												<dg-page-forgot-password
+												@signup=${(evt) => this.page = SIGNUP}
+												@cancel=${(evt) => this.page = LOGIN}
+												></dg-page-forgot-password>
+											`;
+										}
+										case SIGNUP: {
+											return html`
+												<dg-page-signup
+												@signup=${(evt) => this.page = SIGNUP}
+												@cancel=${(evt) => this.page = LOGIN}
+												></dg-page-signup>
+											`;
+										}
 									}
-									case RESET: {
-										return html`
-											<dg-page-forgot-password
-											@signup=${(evt) => this.page = SIGNUP}
-											@cancel=${(evt) => this.page = LOGIN}
-											></dg-page-forgot-password>
-										`;
-									}
-									case SIGNUP: {
-										return html`
-											<dg-page-signup
-											@signup=${(evt) => this.page = SIGNUP}
-											@cancel=${(evt) => this.page = LOGIN}
-											></dg-page-signup>
-										`;
-									}
-								}
-							})()}
+								})()}
+							</div>
 						</div>
 					</div>
 
