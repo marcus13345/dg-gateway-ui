e766318c37b7171839a2901e60e98272a32e038e
Marcus <marcus@valnet.xyz>
2020-10-12 15:08:37 -0400

    disable data temporarily

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,13 +1,13 @@
 import xGraphAdapter from 'xgraph-adapter';
 
-class Thing extends xGraphAdapter {
-	constructor() {
-		super('52.90.34.213', 27001);
-		console.log('ayayayyayayayaya');
-		this.send('GetState', {}, (err, cmd) => {
-			console.log(err, cmd);
-		});
-	}
-}
+// class Thing extends xGraphAdapter {
+// 	constructor() {
+// 		super('52.90.34.213', 27001);
+// 		console.log('ayayayyayayayaya');
+// 		this.send('GetState', {}, (err, cmd) => {
+// 			console.log(err, cmd);
+// 		});
+// 	}
+// }
 
-console.log(new Thing())
+// console.log(new Thing())
